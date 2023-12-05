1b998854293d4fdcf2a36cdc1147b7084c576cbb
Jerome Bobin <jeromebob49.3@gmail.com>
2023-12-05 15:17:13 +0100

    js englobed by 'document.addEventListener('DOMContentLoaded', function()'

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-const items = document.querySelectorAll('img');
-console.log('items', items);
-const nbSlide = items.length;
-console.log('nbSlide', nbSlide);
-const suivant = document.querySelector('.right');
-const precedent = document.querySelector('.left');
-console.log('arrow', suivant, precedent);
-let count = 0;
-
-function slideSuivante(){
-    items[count].classList.remove('active');
-
-    if(count < nbSlide - 1){
-        count++;
-    } else {
-        count = 0;
-    }
-
-    items[count].classList.add('active')
-    console.log(count);
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('img');
+    console.log('items', items);
+    const nbSlide = items.length;
+    console.log('nbSlide', nbSlide);
+    const suivant = document.querySelector('.right');
+    const precedent = document.querySelector('.left');
+    console.log('arrow', suivant, precedent);
+    let count = 0;
     
-}
-suivant.addEventListener('click', slideSuivante)
-
-
-function slidePrecedente(){
-    items[count].classList.remove('active');
-
-    if(count > 0){
-        count--;
-    } else {
-        count = nbSlide - 1;
-    }
-
-    items[count].classList.add('active')
-    // console.log(count);
+    function slideSuivante(){
+        items[count].classList.remove('active');
     
-}
-precedent.addEventListener('click', slidePrecedente)
-
-function keyPress(e){
-    console.log(e);
+        if(count < nbSlide - 1){
+            count++;
+        } else {
+            count = 0;
+        }
     
-    if(e.keyCode === 37){
-        slidePrecedente();
-    } else if(e.keyCode === 39){
-        slideSuivante();
+        items[count].classList.add('active')
+        console.log(count);
+        
     }
-}
-document.addEventListener('keydown', keyPress)
+    suivant.addEventListener('click', slideSuivante)
+    
+    
+    function slidePrecedente(){
+        items[count].classList.remove('active');
+    
+        if(count > 0){
+            count--;
+        } else {
+            count = nbSlide - 1;
+        }
+    
+        items[count].classList.add('active')
+        // console.log(count);
+        
+    }
+    precedent.addEventListener('click', slidePrecedente)
+    
+    function keyPress(e){
+        console.log(e);
+        
+        if(e.keyCode === 37){
+            slidePrecedente();
+        } else if(e.keyCode === 39){
+            slideSuivante();
+        }
+    }
+    document.addEventListener('keydown', keyPress)
+  });
